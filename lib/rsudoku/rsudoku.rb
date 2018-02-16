b1909b18610fa939f5df048e561c9a2d9e102dd9
Louis Alridge <loualrid@gmail.com>
2018-02-15 20:18:15 -0800

    Rsudoku initializer

--- lib/rsudoku/rsudoku.rb
+++ lib/rsudoku/rsudoku.rb
@@ -1,5 +1,17 @@
 class RSudoku
-  def initialize
-
+  # Creates a new RSudoku object
+  #
+  # @param [Object] obj A multi-dim array representing an unsolved sudoku board,
+  #   in the format [[[1,2, nil], [4, nil, nil], nil, 8, 9], [1, nil, 3], ...], or nil
+  #
+  # @return [RSudoku]
+  #
+  # @example
+  #   RSudoku.new(ARR)
+  #   RSudoku.new
+  #   Money.new(100, "EUR") #=> #<Money @fractional=100 @currency="EUR">
+  #
+  def initialize(obj = nil)
+    @board = obj
   end
 end
---
+++ spec/rsudoku_spec.rb
@@ -0,0 +1,9 @@
+describe RSudoku do
+  describe ".new" do
+    subject(:rsudoku) { RSudoku.new }
+
+    it "should build a 3x3 assortment of boards as a standard sudoku board" do
+      expect(rsudoku.boards.count).to be 9
+    end
+  end
+end
