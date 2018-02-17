ff83af344e41dd22d86ec407e6d2fc6d9deabff1
Louis Alridge <loualrid@gmail.com>
2018-02-17 13:49:34 -0800

    More work on initialization features

--- lib/rsudoku/rsudoku.rb
+++ lib/rsudoku/rsudoku.rb
@@ -2,16 +2,47 @@ class RSudoku
   # Creates a new RSudoku object
   #
   # @param [Object] obj A multi-dim array representing an unsolved sudoku board,
-  #   in the format [[[1,2, nil], [4, nil, nil], nil, 8, 9], [1, nil, 3], ...], or nil
+  #   in the format [[[1,2, nil], [4, nil, nil], nil, 8, 9], [1, nil, 3], ...],
+  #   or nil
   #
   # @return [RSudoku]
   #
   # @example
   #   RSudoku.new(ARR)
   #   RSudoku.new
-  #   Money.new(100, "EUR") #=> #<Money @fractional=100 @currency="EUR">
   #
   def initialize(obj = nil)
     @board = obj
+  end
+
+  # @!attribute [r] boards
+  #   @return [boards] An array of the sudoku boards.
+
+  attr_reader :boards
+
+  class << self
+
+    # @!attribute [rw] default_board_size
+    #   @return [Integer] Use this to specify the default board size for new
+    #     sudoku boards
+    attr_accessor :default_board_size
+  end
+
+  def self.setup_defaults
+    # Default to 3x3 Boards
+    self.default_board_size = :three_by_three
+  end
+
+  def self.inherited(base)
+    base.setup_defaults
+  end
+
+  setup_defaults
+
+  # Common inspect function
+  #
+  # @return [String]
+  def inspect
+    "#<#{self.class.name}"
   end
 end
